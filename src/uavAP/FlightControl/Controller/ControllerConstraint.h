ef8c70c7d919c28de4880ee0f441921a4909ecf1
Simon Yu <yujunda@icloud.com>
2019-03-19 19:30:18 -0500

    Added additional controller constraints.

--- src/uavAP/FlightControl/Controller/ControllerConstraint.h
+++ src/uavAP/FlightControl/Controller/ControllerConstraint.h
@@ -12,11 +12,23 @@
 
 enum class ControllerConstraints
 {
-	INVALID, ROLL, ROLL_RATE, PITCH, PITCH_RATE, NUM_CONSTRAINT
+	INVALID,
+	ROLL,
+	ROLL_RATE,
+	ROLL_OUTPUT,
+	PITCH,
+	PITCH_RATE,
+	PITCH_OUTPUT,
+	YAW_OUTPUT,
+	THROTTLE_OUTPUT,
+	NUM_CONSTRAINT
 };
 
 ENUMMAP_INIT(ControllerConstraints, { {ControllerConstraints::ROLL, "roll"},
-		{ControllerConstraints::ROLL_RATE, "roll_rate"}, {ControllerConstraints::PITCH, "pitch"},
-		{ControllerConstraints::PITCH_RATE, "pitch_rate"} });
+		{ControllerConstraints::ROLL_RATE, "roll_rate"}, {ControllerConstraints::ROLL_OUTPUT,
+		"roll_output"}, {ControllerConstraints::PITCH, "pitch"}, {ControllerConstraints::PITCH_RATE,
+		"pitch_rate"}, {ControllerConstraints::PITCH_OUTPUT, "pitch_output"},
+		{ControllerConstraints::YAW_OUTPUT, "yaw_output"}, {ControllerConstraints::THROTTLE_OUTPUT,
+		"throttle_output"} });
 
 #endif /* UAVAP_FLIGHTCONTROL_CONTROLLER_CONTROLLERCONSTRAINT_H_ */
