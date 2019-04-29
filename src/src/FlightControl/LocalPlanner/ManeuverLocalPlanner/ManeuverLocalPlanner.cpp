1679626e7a76826d4fedd689f6979b688f1fd2ad
Mirco Theile <mirco.theile@tum.de>
2019-04-29 16:23:54 +0200

    Macro fix

--- src/src/FlightControl/LocalPlanner/ManeuverLocalPlanner/ManeuverLocalPlanner.cpp
+++ src/src/FlightControl/LocalPlanner/ManeuverLocalPlanner/ManeuverLocalPlanner.cpp
@@ -16,11 +16,11 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 ////////////////////////////////////////////////////////////////////////////////
-/*
- * ManeuverLocalPlanner.cpp
- *
- *  Created on: Aug 2, 2018
- *      Author: mircot
+/**
+ * @file ManeuverLocalPlanner.cpp
+ * @date Aug 2, 2018
+ * @author Mirco Theile, mirco.theile@tum.de
+ * @brief Description
  */
 #include <uavAP/Core/DataHandling/DataHandling.h>
 #include <uavAP/Core/IPC/IPC.h>
